4c3842e2172c3d4e1d642bf1a91d6da0cb2582b2
Denis Komarkov <denis.komarkov@gmail.com>
2025-05-28 23:28:10 +0300

    feat(devtodev): basic devtodev support

--- com.arealczech.sdk.devtodev/Scripts/DevToDev.cs
+++ com.arealczech.sdk.devtodev/Scripts/DevToDev.cs
@@ -1,7 +1,82 @@
+using System;
+using System.Collections.Generic;
+using Areal.SDK.Common;
+using DevToDev.Analytics;
+
 namespace Areal.SDK {
-    public class DevToDev {
-        public DevToDev() {
-            
+    public class DevToDev : ITutorialAnalyticsService, ICustomEventAnalyticsService, ILevelUpAnalyticsService {
+        public DevToDev(
+            string androidAppId = null,
+            string iosAppId = null,
+            string webAppId = null,
+            string winAppId = null,
+            string osxAppId = null,
+            string uwpAppId = null) {
+            string token =
+#if UNITY_ANDROID
+                androidAppId;
+#elif UNITY_IOS
+                iosAppId;
+#elif UNITY_WEBGL
+                webAppId;
+#elif UNITY_STANDALONE_WIN
+                winAppId;
+#elif UNITY_STANDALONE_OSX
+                osxAppId;
+#elif UNITY_WSA
+                uwpAppId;
+#else
+                null;
+#endif
+
+            if (token == null) {
+                throw new ArgumentException("No App ID provided for the current platform.");
+            }
+
+            DTDAnalytics.Initialize(token);
+        }
+
+        public void LogTutorialStart() {
+            DTDAnalytics.Tutorial(-1);
+        }
+
+        public void LogTutorialStep(int step) {
+            DTDAnalytics.Tutorial(step);
+        }
+
+        public void LogTutorialFinish() {
+            DTDAnalytics.Tutorial(-2);
+        }
+
+        public void LogTutorialSkipped() {
+            DTDAnalytics.Tutorial(0);
+        }
+
+        public void LogCustomEvent(string eventName, params KeyValuePair<string, object>[] parameters) {
+            var convertedParameters = new DTDCustomEventParameters();
+
+            foreach (var (key, value) in parameters) {
+                switch (value) {
+                    case byte or sbyte or short or ushort or int or uint or long or ulong:
+                        convertedParameters.Add(key, Convert.ToInt64(value));
+                        break;
+                    case double or float:
+                        convertedParameters.Add(key, Convert.ToDouble(value));
+                        break;
+                    case bool boolValue:
+                        convertedParameters.Add(key, boolValue);
+                        break;
+                    default:
+                        convertedParameters.Add(key, value.ToString());
+                        break;
+                }
+            }
+
+            DTDAnalytics.CustomEvent(eventName, convertedParameters);
+        }
+
+        public void LogLevelUp(int level) {
+            DTDAnalytics.LevelUp(level);
         }
     }
 }
