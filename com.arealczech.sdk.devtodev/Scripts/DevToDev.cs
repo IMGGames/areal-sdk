9ed8bf6be8eb5d38d3872e0f17c801a6b11ef3f0
Denis Komarkov <denis.komarkov@gmail.com>
2025-07-24 01:14:14 +0300

    feat(devtodev): implement IPurchaseAnalyticsService and IVirtualCurrencyAnalyticsService

--- com.arealczech.sdk.devtodev/Scripts/DevToDev.cs
+++ com.arealczech.sdk.devtodev/Scripts/DevToDev.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Areal.SDK.Common;
+using Areal.SDK.Common.Enums;
 using DevToDev.Analytics;
 
 namespace Areal.SDK {
-    public class DevToDev : ITutorialAnalyticsService, ICustomEventAnalyticsService, ILevelUpAnalyticsService {
+    public class DevToDev : ITutorialAnalyticsService, ICustomEventAnalyticsService, ILevelUpAnalyticsService, IPurchaseAnalyticsService,
+        IVirtualCurrencyAnalyticsService {
         // ReSharper disable UnusedParameter.Local
         public DevToDev(
             string androidAppId = null,
@@ -80,6 +83,41 @@ namespace Areal.SDK {
 
         public void LogLevelUp(int level) {
             DTDAnalytics.LevelUp(level);
+        }
+
+        public void LogPurchaseInitiation(string productId) {
+            // ignored
+        }
+
+        public void LogPurchase(string productId, string transactionId, string isoCurrencyCode, decimal price) {
+            DTDAnalytics.RealCurrencyPayment(transactionId, (double)price, productId, isoCurrencyCode);
+        }
+
+        public void LogCurrentBalance(Dictionary<string, long> resources) {
+            DTDAnalytics.CurrentBalance(resources);
+        }
+
+        public void LogVirtualCurrencyAccrual(string source, AccrualType accrualType, Dictionary<string, long> resources) {
+            foreach (var (currency, amount) in resources) {
+                DTDAnalytics.CurrencyAccrual(currency, (int)amount, source, ConvertAccrualTypeToDevToDev(accrualType));
+            }
+        }
+
+        private static DTDAccrualType ConvertAccrualTypeToDevToDev(AccrualType type) {
+            return type switch {
+                AccrualType.Earned => DTDAccrualType.Earned,
+                AccrualType.Bought => DTDAccrualType.Bought,
+                _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+            };
+        }
+
+        public void LogVirtualCurrencyPayment(string purchaseId, string purchaseCategory, int purchaseAmount, Dictionary<string, long> resources) {
+            DTDAnalytics.VirtualCurrencyPayment(
+                purchaseId,
+                purchaseCategory,
+                purchaseAmount,
+                resources.ToDictionary(e => e.Key, e => (int)e.Value)
+            );
         }
     }
 }
