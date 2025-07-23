90d72bbcbc04cac3ba0a30eea28ea291416fe03b
Denis Komarkov <denis.komarkov@gmail.com>
2025-07-24 01:33:12 +0300

    feat(adapter): purchase & virtual currency services

--- com.arealczech.sdk.analytics.adapter/Scripts/Analytics.cs
+++ com.arealczech.sdk.analytics.adapter/Scripts/Analytics.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Areal.SDK.Common;
+using Areal.SDK.Common.Enums;
 using UnityEngine;
 
 namespace Areal.SDK {
@@ -10,11 +11,15 @@ namespace Areal.SDK {
         private static ICustomEventAnalyticsService[] _customEventServices;
         private static ITutorialAnalyticsService[] _tutorialServices;
         private static ILevelUpAnalyticsService[] _levelUpServices;
+        private static IPurchaseAnalyticsService[] _purchaseServices;
+        private static IVirtualCurrencyAnalyticsService[] _virtualCurrencyServices;
 
         public static void Init(params IAnalyticsService[] services) {
             _customEventServices = services.OfType<ICustomEventAnalyticsService>().ToArray();
             _tutorialServices = services.OfType<ITutorialAnalyticsService>().ToArray();
             _levelUpServices = services.OfType<ILevelUpAnalyticsService>().ToArray();
+            _purchaseServices = services.OfType<IPurchaseAnalyticsService>().ToArray();
+            _virtualCurrencyServices = services.OfType<IVirtualCurrencyAnalyticsService>().ToArray();
         }
 
         public static void LogCustomEvent(string eventName, params KeyValuePair<string, object>[] parameters) {
@@ -81,6 +86,61 @@ namespace Areal.SDK {
 
             foreach (var service in _levelUpServices) {
                 service.LogLevelUp(level);
+            }
+        }
+
+        public static void LogPurchaseInitiation(string productId) {
+            if (_purchaseServices is not { Length: > 0 }) {
+                Debug.LogWarning($"{Prefix} {nameof(LogPurchaseInitiation)}: no {nameof(IPurchaseAnalyticsService)} provided");
+                return;
+            }
+
+            foreach (var service in _purchaseServices) {
+                service.LogPurchaseInitiation(productId);
+            }
+        }
+
+        public static void LogPurchase(string productId, string transactionId, string isoCurrencyCode, decimal price) {
+            if (_purchaseServices is not { Length: > 0 }) {
+                Debug.LogWarning($"{Prefix} {nameof(LogPurchase)}: no {nameof(IPurchaseAnalyticsService)} provided");
+                return;
+            }
+
+            foreach (var service in _purchaseServices) {
+                service.LogPurchase(productId, transactionId, isoCurrencyCode, price);
+            }
+        }
+
+        public static void LogCurrentBalance(Dictionary<string, long> resources) {
+            if (_purchaseServices is not { Length: > 0 }) {
+                Debug.LogWarning($"{Prefix} {nameof(LogCurrentBalance)}: no {nameof(IVirtualCurrencyAnalyticsService)} provided");
+                return;
+            }
+
+            foreach (var service in _virtualCurrencyServices) {
+                service.LogCurrentBalance(resources);
+            }
+        }
+
+        public static void LogVirtualCurrencyAccrual(string source, AccrualType accrualType, Dictionary<string, long> resources) {
+            if (_purchaseServices is not { Length: > 0 }) {
+                Debug.LogWarning($"{Prefix} {nameof(LogVirtualCurrencyAccrual)}: no {nameof(IVirtualCurrencyAnalyticsService)} provided");
+                return;
+            }
+
+            foreach (var service in _virtualCurrencyServices) {
+                service.LogVirtualCurrencyAccrual(source, accrualType, resources);
+            }
+        }
+
+        public static void LogVirtualCurrencyPayment(string purchaseId, string purchaseCategory, int purchaseAmount, Dictionary<string, long> resources) {
+            if (_purchaseServices is not { Length: > 0 }) {
+                Debug.LogWarning($"{Prefix} {nameof(LogVirtualCurrencyPayment)}: no {nameof(IVirtualCurrencyAnalyticsService)} provided");
+                return;
+            }
+
+            foreach (var service in _virtualCurrencyServices) {
+                service.LogVirtualCurrencyPayment(purchaseId, purchaseCategory, purchaseAmount, resources);
             }
         }
     }
